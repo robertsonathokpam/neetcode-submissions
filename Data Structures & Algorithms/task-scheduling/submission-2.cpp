52a7f3af6682922319a15d959afd49cbfd52c365
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-03 22:48:21 +0530

    Add: task-scheduling - submission-2

---
+++ Data Structures & Algorithms/task-scheduling/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        map<char,int>mp;
+        for(char c:tasks){
+            mp[c]++;
+        }
+        int maxi=0;
+        for(auto it:mp){
+            maxi=max(maxi,it.second);
+        }
+        int cnt=0;
+        for(auto it:mp){
+            if(it.second==maxi){
+                cnt++;
+            }
+        }
+            int x=n+1;
+            int y=maxi-1;
+            int res=x*y+cnt;
+            return max((int)tasks.size(),res);
+    }
+};
