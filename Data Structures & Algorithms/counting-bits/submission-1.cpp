cbe36f384a2120f194c22ccb7355d985da4ea8d4
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-04 11:54:24 +0530

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int>nums(n+1);
+        int cnt=0;
+        for(int i=0;i<=n;i++){
+            int z=i;
+            cnt=0;
+            for(int j=0;j<10;j++){
+                if((1<<j)&z){
+                    cnt++;
+                }
+            }
+            nums[i]=cnt;
+        }
+        return nums;
+    }
+};
