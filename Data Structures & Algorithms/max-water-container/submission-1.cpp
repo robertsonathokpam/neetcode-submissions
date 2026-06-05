95d2a68bde4e7e4d4c4e68d61da0495b6835040a
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-05 16:03:42 +0530

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxArea(vector<int>& h) {
+        int n=h.size();
+        int l=0,r=n-1;
+        int area=0;
+        while(l<r){
+            int k=min(h[l],h[r]);
+            k*=(r-l);
+            area=max(area,k);
+            if(h[l]>h[r]){
+                r--;
+            }else l++;
+        }
+        return area;
+    }
+};
