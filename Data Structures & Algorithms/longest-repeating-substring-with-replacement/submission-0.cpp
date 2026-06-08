75bc16dfe71f5699b737fc6a28d08fe1afc0a4d2
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-08 19:50:57 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool fn(map<char,int>& mp,int k){
+        int cnt=0;
+        int maxi=0;
+        char c='.'; 
+        for(auto [k,v]:mp){
+            if(v>maxi){
+                maxi=v;
+                c=k;
+            }
+        }
+        for(auto [k,v]:mp){
+            if(c!=k){
+                cnt+=v;
+            }
+        }
+        return cnt<=k;
+    }
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        map<char,int>mp;
+        int res=0;
+        int l=0;
+        for(int r=0;r<n;r++){
+           mp[s[r]]++;
+           while(!fn(mp,k)){
+              mp[s[l]]--;
+              if(mp[s[l]]==0) mp.erase(s[l]);
+              l++;
+           }
+           res=max(res,r-l+1);
+        }
+        return res;
+    }
+};
