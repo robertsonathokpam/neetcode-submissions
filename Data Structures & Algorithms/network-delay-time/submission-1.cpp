47b83959d4687746ffda031ce001754d3717e2c3
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-11 11:43:50 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>>adj(n+1);
+        for(int i=0;i<times.size();i++){
+            adj[times[i][0]].push_back({times[i][1],times[i][2]});
+        }
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>dist(n+1,INT_MAX);
+        pq.push({0,k});
+        dist[k]=0;
+        while(!pq.empty()){
+            auto [w,u]=pq.top();
+            pq.pop();
+            for(auto [v,nw]:adj[u]){
+                if(dist[v]>nw+w){
+                    dist[v]=nw+dist[u];
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+        int res=-1;
+        for(int i=1;i<=n;i++){
+            res=max(res,dist[i]);
+        }
+
+        if(res==INT_MAX) return -1;
+        return res;
+    }
+};
