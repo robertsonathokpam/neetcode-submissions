977ae834d7b45eabdd4656925eaf44cfae28becf
Robertson Athokpam <robertsonathokpam1234@gmail.com>
2026-06-13 10:48:49 +0530

    Add: search-for-word - submission-4

---
+++ Data Structures & Algorithms/search-for-word/submission-4.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<vector<char>>b;
+    string s;
+    int n,m;
+    vector<vector<int>>vis;
+    bool dfs(int r,int c,int ind){
+      
+       if(ind==s.size()){
+          return true;
+       }
+       if(r>=n || c>=m || r<0 || c<0) return false;
+       if(vis[r][c]) return false;
+       if(b[r][c]!=s[ind]) return false;
+       
+       vector<int>dc={-1,0,1,0};
+       vector<int>dr={0,-1,0,1};
+       bool k=false;
+       vis[r][c]=1;
+       for(int i=0;i<4;i++){
+          int nr=r+dr[i];
+          int nc=c+dc[i];
+          k |=dfs(nr,nc,ind+1);
+       }
+       vis[r][c]=0;
+       return k;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        b=board;
+        n=b.size();
+        m=b[0].size();
+        vis.assign(n,vector<int>(m,0));
+        s=word;
+        for(int i=0;i<n;i++){
+         for(int j=0;j<m;j++){
+            if(dfs(i,j,0)) return true;
+         }
+        }
+        return false;
+    }
+};
